71afe4061fbbb58f05861166eeee6dc293f419f5
ESE\peter.mahady <peter.mahady@intertek.com>
2018-07-17 17:00:28 +0200

    Add some icons to sidenav bar

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -7,7 +7,8 @@ import { MatButtonModule,
          MatNativeDateModule,
          MatCheckboxModule,
          MatSidenavModule,
-         MatToolbarModule} from '@angular/material';
+         MatToolbarModule,
+         MatListModule} from '@angular/material';
 
 @NgModule({
     imports: [
@@ -19,7 +20,8 @@ import { MatButtonModule,
         MatNativeDateModule,
         MatCheckboxModule,
         MatSidenavModule,
-        MatToolbarModule
+        MatToolbarModule,
+        MatListModule
     ],
     exports: [
         MatButtonModule, 
@@ -30,7 +32,8 @@ import { MatButtonModule,
         MatNativeDateModule,
         MatCheckboxModule,
         MatSidenavModule,
-        MatToolbarModule
+        MatToolbarModule,
+        MatListModule
     ]
 })
 export class MaterialModule{}

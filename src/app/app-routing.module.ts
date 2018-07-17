97ffef7795cb2d3c30c42f3668ea71789d3751eb
ESE\peter.mahady <peter.mahady@intertek.com>
2018-07-17 15:37:56 +0200

    add the login form with a reactive form i.e. created programmatically to the project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ import { TrainingComponent } from "./training/training.component";
 const routes: Routes = [
     {path: '', component: WelcomeComponent},
     {path: 'signup', component: SignupComponent},
-    {path: 'login', component: SignupComponent},
+    {path: 'login', component: LoginComponent},
     {path: 'training', component: TrainingComponent}
 ]
 

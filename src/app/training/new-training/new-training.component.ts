402961042a348a95b32940f63b90e6f56c03a2bb
ESE\peter.mahady <peter.mahady@intertek.com>
2018-07-18 15:17:52 +0200

    hook the training service up to the new exercise component

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { TrainingService } from '../training.service';
+import { Exercise } from '../exercise.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-new-training',
@@ -6,14 +10,15 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./new-training.component.css']
 })
 export class NewTrainingComponent implements OnInit {
+  exercises: Exercise[];
 
-  @Output() trainingStart = new EventEmitter<void>();
-  constructor() { }
+  constructor(private trainingService: TrainingService) { }
 
   ngOnInit() {
+    this.exercises = this.trainingService.getAvailableExercises();
   }
 
-  onStartTraining(){
-    this.trainingStart.emit();
+  onStartTraining(form: NgForm){
+    this.trainingService.startExercise(form.value.exercise);
   }
 }
--- src/app/training/training.component.ts
+++ src/app/training/training.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { TrainingService } from './training.service';
 
 @Component({
   selector: 'app-training',
   templateUrl: './training.component.html',
   styleUrls: ['./training.component.css']
 })
-export class TrainingComponent implements OnInit {
+export class TrainingComponent implements OnInit, OnDestroy {
   ongoingTraining = false;
-  constructor() { }
+  trainingSubscription: Subscription
+  constructor(private trainingService: TrainingService) { }
 
   ngOnInit() {
+    this.trainingSubscription = this.trainingService.exerciseChanged.subscribe( ex => {
+      if(ex){
+        this.ongoingTraining=true;
+      }
+      else{
+        this.ongoingTraining=false;
+      }
+    });
+  }
+
+  ngOnDestroy(){
+    this.trainingSubscription.unsubscribe();
   }
 
 }

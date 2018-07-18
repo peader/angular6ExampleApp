1122b84f337125595f2e924fcddb9287f3c4a41d
ESE\peter.mahady <peter.mahady@intertek.com>
2018-07-18 14:14:28 +0200

    add authguards and ngifs to the navigation based on whether user is logged in or not

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,18 +1,24 @@
 import { Subject } from 'rxjs/Subject';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { User } from "./user.model";
 import { AuthData } from "./auth.data";
 
+
+@Injectable()
 export class AuthService {
     authChange= new Subject<boolean>();
     private user: User;
+
+    constructor(private router: Router) {}
 
     registerUser(authData: AuthData){
         this.user = {
             email: authData.email,
             userId: Math.round(Math.random() * 10000).toString()
         };
-        this.authChange.next(true);
+        this.authSuccessFully();
     }
 
     login(authData: AuthData){
@@ -20,12 +26,13 @@ export class AuthService {
             email: authData.email,
             userId: Math.round(Math.random() * 10000).toString()
         };
-        this.authChange.next(true);
+        this.authSuccessFully();
     }
 
     logout(){
         this.user = null;
         this.authChange.next(false);
+        this.router.navigate(['/login']);
     }
 
     getUser(){
@@ -34,5 +41,10 @@ export class AuthService {
 
     isAuth() {
         return this.user != null;
+    }
+
+    private authSuccessFully(){
+        this.authChange.next(true);
+        this.router.navigate(['/training']);
     }
 }
